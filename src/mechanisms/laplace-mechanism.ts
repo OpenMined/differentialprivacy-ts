3a97a3f49f4983ad8bd60cd2084fe1d70ae43ff3
Ben <ben.szymkow@cognitivo.com.au>
2020-05-17 14:16:36 +1000

    stdlib

--- src/mechanisms/laplace-mechanism.ts
+++ src/mechanisms/laplace-mechanism.ts
@@ -1,11 +1,10 @@
 import {
-  Result,
-  StatusCode,
-  PerturbationMechanism,
+    Result,
+    StatusCode,
+    PerturbationMechanism,
 } from './perturbation-mechanism';
 
 class LaplaceMechanism<T extends number> extends PerturbationMechanism<T> {
-
   get Epsilon(): number {
     return this._epsilon;
   }
@@ -22,47 +21,49 @@ class LaplaceMechanism<T extends number> extends PerturbationMechanism<T> {
     super();
     this._epsilon = 0.3; // Kritika to very this
     this._delta = 0;
-
-  }
-
-  private exponentialSample = (mean: number): number => {
-    return (mean*-1)*Math.log(Math.random())
   }
 
   addNoise(sensitivity: number, queryResult: T): Result<T> {
-    // 1) sample from laplace distribution
-    let noise = this.exponentialSample(sensitivity) - this.exponentialSample(sensitivity)
+        // 1) sample from laplace distribution
+    const noise =
+            this.exponentialSample(sensitivity) -
+            this.exponentialSample(sensitivity);
 
-    // 2) add that noise to the query result
-    let result = queryResult + noise
+        // 2) add that noise to the query result
+    const result = queryResult as number + noise;
 
-    // 3) Validate result is within bounds
-    if(result < this.getLowerBounds() || result > this.getUpperBounds()) {
-      this._currentStatus = StatusCode.OutOfRange
+        // 3) Validate result is within bounds
+    if (result < this.getLowerBounds() || result > this.getUpperBounds()) {
+      this._currentStatus = StatusCode.OutOfRange;
+
       return this._currentStatus;
     }
 
-    // 4) if bounds not exceeded:
-    // 4.1) Update privacy budget,
+        // 4) if bounds not exceeded:
+        // 4.1) Update privacy budget,
 
-    // 4.1) set CurrentStatus to SuccessfullyPerturbed
+        // 4.1) set CurrentStatus to SuccessfullyPerturbed
     this._currentStatus = StatusCode.SuccessfullyPerturbed;
-    // 4.2) update privacy budget
+        // 4.2) update privacy budget
 
-    // 5.3) if privacy budget exceeded set CurrentStatus to RunOutOfBudget and return that value
+        // 5.3) if privacy budget exceeded set CurrentStatus to RunOutOfBudget and return that value
 
-    // 5.4) return result
+        // 5.4) return result
     return result;
   }
 
-  // Ria to research best approach
+    // Ria to research best approach
   protected getLowerBounds = (): number => {
     return 0;
   }
 
-  // Ria to research best approach
+    // Ria to research best approach
   protected getUpperBounds = (): number => {
     return 0;
+  }
+
+  private readonly exponentialSample = (mean: number): number => {
+    return mean * -1 * Math.log(Math.random());
   }
 }
 
